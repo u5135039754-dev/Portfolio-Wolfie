e6cf3913a9d19353e68d09e415192af1732bc372
Your Name <u513509754@gmail.com>
2026-04-17 10:17:25 -0700

    solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { Home } from "./Components/Home";
 
 export const Root = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="" element={<Home />} />
       </Route>
       {/* <Route path="*" element={<ErrorNotification />} /> */}
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
